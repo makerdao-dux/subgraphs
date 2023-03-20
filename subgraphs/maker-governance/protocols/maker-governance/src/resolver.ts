f8259a1fbf3bcfa98b7476307b7cd6ad06ad4291
Rafael Ventura <rafinskipg@gmail.com>
2023-03-20 15:47:10 +0100

    Define metadata

--- subgraphs/maker-governance/protocols/maker-governance/src/resolver.ts
+++ subgraphs/maker-governance/protocols/maker-governance/src/resolver.ts
@@ -1,3 +1,4 @@
+import { json } from "@graphprotocol/graph-ts";
 import { ContenthashChanged } from "../../../generated/Resolver/Resolver";
 import { ContentHashRegistry } from "../../../generated/schema";
 
@@ -10,15 +11,24 @@ export function handleContentHashChanged(event: ContenthashChanged): void {
   const transactionID = event.transaction.hash;
 
   const registry = new ContentHashRegistry(
-    node.toString() + "-" + contentHash.toHexString()
+    node.toHexString() + "-" + contentHash.toHexString()
   );
+
   registry.node = node;
-  registry.nodeHexString = node.toString();
-  registry.hashHexString = contentHash.toString();
+  registry.nodeHexString = node.toHexString();
+  registry.hashHexString = contentHash.toHexString();
   registry.blockNumber = blockNumber;
   registry.transactionID = transactionID;
   registry.hash = contentHash;
   registry.address = address;
+
+  // Fetch the latest content hash from IPFS
+  // first we need to get the IPFS hash from the content hash, it is encoded in b58
+  // let manifest = ipfs.cat(registry.hash.toHexString());
+
+  // Parse the latest content hash
+  //let parsed = json.fromBytes(manifest).toObject();
+
   registry.save();
   // TODO: Store a entity with the latest content hash for our ens domain
 }
