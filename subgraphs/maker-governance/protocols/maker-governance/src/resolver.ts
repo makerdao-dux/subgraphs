99bd5697f5ff3d3bebaf3a43888efb7dcea0bab0
Rafael Ventura <rafinskipg@gmail.com>
2023-03-20 12:04:05 +0100

    Try

--- subgraphs/maker-governance/protocols/maker-governance/src/resolver.ts
+++ subgraphs/maker-governance/protocols/maker-governance/src/resolver.ts
@@ -1,11 +1,24 @@
-export function handleContentHashChanged(event: ContenthashChangedEvent): void {
-  // Filter by our ens domain
+import { ContenthashChanged } from "../../../generated/Resolver/Resolver";
+import { ContentHashRegistry } from "../../../generated/schema";
 
+export function handleContentHashChanged(event: ContenthashChanged): void {
+  // Filter by our ens domain
   const node = event.params.node;
   const address = event.address;
   const contentHash = event.params.hash;
   const blockNumber = event.block.number.toI32();
   const transactionID = event.transaction.hash;
 
+  const registry = new ContentHashRegistry(
+    node.toHexString() + "-" + contentHash.toHexString()
+  );
+  registry.node = node;
+  registry.nodeHexString = node.toHexString();
+  registry.hashHexString = contentHash.toHexString();
+  registry.blockNumber = blockNumber;
+  registry.transactionID = transactionID;
+  registry.hash = contentHash;
+  registry.address = address;
+  registry.save();
   // TODO: Store a entity with the latest content hash for our ens domain
 }
